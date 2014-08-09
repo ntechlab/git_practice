97d99a5489d2b297b86c15725fe3c55d56111813
tnog <tnog@example.com>
2014-08-10 04:11:28 +0900

    暫定コミット

---
+++ MySailsApp/auth01/api/controllers/AuthController.js
@@ -0,0 +1,37 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing Auths
+ * @help :: See http://links.sailsjs.org/docs/controllers
+ */
+
+var passport = require("passport");
+
+module.exports = {
+	login : function(req, res) {
+		console.log("login action");
+		res.view("auth/login");
+	},
+
+	process : function(req, res) {
+		console.log("process action");
+		passport.authenticate('local', function(err, user, info) {
+			if ((err) || (!user)) {
+				res.redirect('/login');
+				return;
+			}
+			req.logIn(user, function(err) {
+				if (err) {
+					res.redirect('/login');
+				}
+				return res.redirect('/');
+			});
+		})(req, res);
+	},
+
+	logout : function(req, res) {
+		req.logout();
+		res.send('logout successful');
+	},
+	_config : {}
+};

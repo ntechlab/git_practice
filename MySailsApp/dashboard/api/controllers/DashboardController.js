0f7bd214b0a2ce75b4dc8c82c791b2478fcd9df2
ay <ay@example.com>
2014-08-21 17:17:15 +0900

    付箋ボードに遷移するボタン追加

--- MySailsApp/dashboard/api/controllers/DashboardController.js
+++ MySailsApp/dashboard/api/controllers/DashboardController.js
@@ -19,7 +19,9 @@ module.exports = {
 	console.log("selected boardId:"+boardId);
 
 	var wait = function (callbacks, done) {
+
 	    var counter = callbacks.length;
+	if(counter > 0){
 	    var next = function() {
 		if (--counter == 0) {
 		    done();
@@ -29,6 +31,81 @@ module.exports = {
 	    for (var i=0; i < callbacks.length; i++) {
 		callbacks[i](next);
 	    };
+	} else {
+	done();
+	}
+	}
+
+	Board.findOne(boardId).exec(function(err,found){
+	    console.log("edit board:found["+found+"]");
+	    Ticket.find({boardId : boardId}).exec(function(err, tickets) {
+
+		// 第1段階で終了すべき関数
+		var prerequisite = [];
+
+		// 対象ボード上のチケットにニックネームを追加する。
+		for(var i = 0; i < tickets.length; i++) {
+
+		    // インデックス番号を保持するためのクロージャー
+		    new function(num){
+			var ti = tickets[num];
+
+			// 各チケットに対するニックネーム取得処理のラッパ関数をprerequisiteに格納
+			prerequisite.push(function(next) {
+			    User.findOne(ti["createUser"]).exec(function(err, userFound) {
+				console.log("prerequisite function No."+num+" called.");
+				if(userFound){
+				    // ニックネームをチケットに追加。
+				    ti["nickname"] = userFound["nickname"];
+				}
+
+				// コールバック関数終了時にnext()を呼び出す。
+				next();
+			    });
+			})
+		    }(i);
+		    console.log("ループ[" + i + "] 終了");
+		};
+
+		// ビュー生成関数のラッパー生成
+		var createViewWrapper = function (){
+		    console.log("createViewWrapper called");
+		    var obj = {boardId: boardId, 
+		 	      userId: userId,
+		 	      title : found["title"], 
+		 	      description: found["description"],
+		 	      list : tickets}; 
+		    console.dir(obj);
+    		    res.view(obj);
+		};
+
+		// 同期処理実行
+		wait(prerequisite, createViewWrapper);
+	    });
+    	});
+    },
+
+    openBoard2 : function(req, res) {
+	var boardId = req.param("selectedId");
+	var userId = req.session.passport.user;
+	console.log("selected boardId:"+boardId);
+
+	var wait = function (callbacks, done) {
+
+	    var counter = callbacks.length;
+	if(counter > 0){
+	    var next = function() {
+		if (--counter == 0) {
+		    done();
+		}
+	    };
+	    
+	    for (var i=0; i < callbacks.length; i++) {
+		callbacks[i](next);
+	    };
+	} else {
+	done();
+	}
 	}
 
 	Board.findOne(boardId).exec(function(err,found){
